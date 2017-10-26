ac121749618f1e0b23ea4b87e394a58f63a16e50
Jasper <jdeweger@live.com>
2017-10-26 07:21:18 -0500

    Added clickableLabel files

---
+++ map-marker/MapMarker/clickableLabel.h
@@ -0,0 +1,17 @@
+
+#ifndef CLICKABLELABEL_H_
+#define CLICKABLELABEL_H_
+#include <QLabel>
+#include <QMouseEvent>
+
+using namespace Qt;
+
+class ClickableLabel:public QLabel
+{
+    Q_OBJECT
+public:
+    ClickableLabel(QWidget *parent = 0);
+    void mousePressEvent(QMouseEvent *eve );
+};
+
+#endif /*CLICKABLELABEL_H_*/
