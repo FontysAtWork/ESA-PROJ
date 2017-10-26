f79d6180b0e76a6625e24c70ab078be7d7281500
Jasper <jdeweger@live.com>
2017-10-26 08:20:43 -0500

    First working yaml implementation

--- map-marker/MapMarker/MapMarker.cpp
+++ map-marker/MapMarker/MapMarker.cpp
@@ -1,9 +1,9 @@
 #include "MapMarker.h"
 #include "ui_MapMarker.h"
 #include "ClickableLabel.h"
-//extern "C" {
-//#include "yaml.h"
-//}
+extern "C" {
+#include "yaml.h"
+}
 
 MapMarker::MapMarker(QWidget *parent) : QMainWindow(parent), ui(new Ui::MapMarker) {
     ui->setupUi(this);
@@ -23,9 +23,9 @@ MapMarker::~MapMarker() {
 
 void MapMarker::on_btnLoadYaml_clicked()
 {
-    /*FILE *fh = fopen("config/public.yaml", "r");
+    FILE *fh = fopen("../maps/legomap-cropped.yaml", "r");
     yaml_parser_t parser;
-
+yaml_token_t  token;   /* new variable */
     // Initialize parser
     if(!yaml_parser_initialize(&parser))
     {
@@ -40,8 +40,32 @@ void MapMarker::on_btnLoadYaml_clicked()
     yaml_parser_set_input_file(&parser, fh);
 
     // CODE HERE
-
+    do {
+        yaml_parser_scan(&parser, &token);
+        switch(token.type)
+        {
+        /* Stream start/end */
+        case YAML_STREAM_START_TOKEN: puts("STREAM START"); break;
+        case YAML_STREAM_END_TOKEN:   puts("STREAM END");   break;
+        /* Token types (read before actual token) */
+        case YAML_KEY_TOKEN:   printf("(Key token)   "); break;
+        case YAML_VALUE_TOKEN: printf("(Value token) "); break;
+        /* Block delimeters */
+        case YAML_BLOCK_SEQUENCE_START_TOKEN: puts("<b>Start Block (Sequence)</b>"); break;
+        case YAML_BLOCK_ENTRY_TOKEN:          puts("<b>Start Block (Entry)</b>");    break;
+        case YAML_BLOCK_END_TOKEN:            puts("<b>End block</b>");              break;
+        /* Data */
+        case YAML_BLOCK_MAPPING_START_TOKEN:  puts("[Block mapping]");            break;
+        case YAML_SCALAR_TOKEN:  printf("scalar %s \n", token.data.scalar.value); break;
+        /* Others */
+        default:
+          printf("Got token of type %d\n", token.type);
+        }
+        if(token.type != YAML_STREAM_END_TOKEN)
+          yaml_token_delete(&token);
+      } while(token.type != YAML_STREAM_END_TOKEN);
+      yaml_token_delete(&token);
     // Cleanup
     yaml_parser_delete(&parser);
-    fclose(fh);*/
+    fclose(fh);
 }
