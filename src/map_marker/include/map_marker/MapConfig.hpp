3def11afd676d6728c077d7d1aa11beef52d0ee8
Jasper <jdeweger@live.com>
2017-11-16 04:58:50 -0600

    Added Mapconfig files and first implementation

---
+++ src/map_marker/include/map_marker/MapConfig.hpp
@@ -0,0 +1,27 @@
+#ifndef YAML_CONFIG_H
+#define YAML_CONFIG_H
+
+#include <string>
+#include "geometry_msgs/Pose.h"
+
+class YamlConfig
+{
+private:
+    std::string imageName;
+    double image_resolution;
+    geometry_msgs::Pose origin;
+    double negate;
+    double occupiedThresh;
+    double freeThresh;
+public:
+    YamlConfig();
+    geometry_msgs::Pose getOrigin();
+    double getResolution();
+    double getImageName();
+    double getOccupiedThresh();
+    double getFreeThresh();
+    double getNegate();
+    void printYaml();
+};
+
+#endif // YAML_CONFIG_H
---
+++ src/map_marker/src/MapConfig.cpp
@@ -0,0 +1,35 @@
+#include "../include/map_marker/MapConfig.hpp"
+    YamlConfig::YamlConfig()
+    {
+
+    }
+    geometry_msgs::Pose YamlConfig::getOrigin()
+    {
+        return origin;
+    }
+    double YamlConfig::getResolution()
+    {
+        return resolution;
+    }
+    double YamlConfig::getImageName()
+    {
+        return imageName;
+    }
+    double YamlConfig::getOccupiedThresh()
+    {
+        return occupiedThresh;
+    }
+    double YamlConfig::getFreeThresh()
+    {
+        return freeThresh;
+    }
+    double YamlConfig::getNegate()
+    {
+        return negate;
+    }
+    void YamlConfig::printYaml()
+    {
+        
+    }
+
+
