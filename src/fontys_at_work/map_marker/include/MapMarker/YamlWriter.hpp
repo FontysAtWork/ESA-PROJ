d1c0193dd107ea5e7b6635c968dab20be9b84a66
Jasper de Weger <jdeweger@live.com>
2017-12-14 15:40:37 -0600

    Added line writing to yamlwriter

--- src/fontys_at_work/map_marker/include/MapMarker/YamlWriter.hpp
+++ src/fontys_at_work/map_marker/include/MapMarker/YamlWriter.hpp
@@ -3,6 +3,7 @@
 #define YAML_WRITER_H
 
 #include "Marker.hpp"
+#include "NoGoLine.hpp"
 #include <vector>
 #include <string>
 
@@ -10,11 +11,13 @@ class YamlWriter
 {
 private:
 	std::string writeMarker(Marker marker);
+	std::string writeLine(NoGoLine line);
 	const std::string positionPrefix = "position_";
 	const std::string orientationPrefix = "orientation_";
 public:
 	YamlWriter();
 	void writeAllMarkers(std::vector<Marker> markers, std::string fileName);
+	void writeAllLines(std::vector<NoGoLine> lines, std::string fileName);
 };
 
 #endif // YAML_WRITER_H
--- src/fontys_at_work/map_marker/src/YamlWriter.cpp
+++ src/fontys_at_work/map_marker/src/YamlWriter.cpp
@@ -35,3 +35,28 @@ std::string YamlWriter::writeMarker(Marker marker)
 	return text;
 
 }
+
+void YamlWriter::writeAllLines(std::vector<NoGoLine> lines, std::string fileName)
+{
+	std::ofstream stream;
+	stream.open(fileName);
+	for (int i = 0; i < lines.size(); ++i)
+	{
+		std::string text = "NoGoLine_" + std::to_string(i) + ": NoGoLine\n";
+		std::string data = writeLine(lines[i]);
+		stream << text << data;
+	}
+	stream.close();
+}
+
+std::string YamlWriter::writeLine(NoGoLine line)
+{
+	std::string text = "Name: " + line.GetName() + "\n";
+	text += "x1: " + std::to_string(line.GetX1()) + "\n";
+	text += "y1: " + std::to_string(line.GetY1()) + "\n";
+	text += "x2: " + std::to_string(line.GetX2()) + "\n";
+	text += "y2: " + std::to_string(line.GetY2()) + "\n";
+	return text;
+
+}
+
