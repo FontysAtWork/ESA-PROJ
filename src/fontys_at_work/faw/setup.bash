c5f73ffd2398039017991f4d38d6233e7a8d05fd
wsneijers <wsneijers@ubuntu-16.04>
2018-01-24 13:47:22 +0100

    removed setup.bash

--- src/fontys_at_work/faw/setup.bash
+++ src/fontys_at_work/faw/setup.bash
@@ -7,13 +7,5 @@ export SIM=false
 
 ##### DO NOT EDIT BELOW THIS LINE #####
 
-# Run the ros setup script
-source devel/setup.bash
-
-export WORKSPACE=`pwd`
-export SRC=$WORKSPACE"/src"
-
 echo "Robot platform       :" $ROBOT
-echo "Workspace folder     :" $WORKSPACE
-echo "Workspace src folder :" $SRC
 echo "Simulation           :" $SIM
